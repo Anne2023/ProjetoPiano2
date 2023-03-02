9067982327fec4758ba27715f36b04da2cda88b2
Amábile Silva <123522722+Anne2023@users.noreply.github.com>
2023-03-02 14:36:39 -0300

    p

--- main.js
+++ main.js
@@ -2,13 +2,23 @@ const pianoKeys = document.querySelectorAll("piano-keys.key"); //por padrão, sc
 
 let audio = new Audio("tunes/a.wav");
 
-const playTune = (key) = >{
+const playTune = (key) => {
+    audio.scr = `tunes/${key}.wav`; //passando src de áudio com base na tecla pressionada
     audio.play(); //tocando áudio
-}
 
+    const clickedKey = document.querySelector(`[data-key=${key}`)
+    clickedKey.classList.add("active");
+}
 pianoKeys.forEach(key =>{
     //chamando a função playtune com a passagem do data-key de dados como um argumento
-
     key.addEventListener("click", () => playTune(key.dataset.key))
     
 });
+
+const pressedKey = (e) => {
+    playTune(e.key);
+}
+
+
+
+document.addEventListener("keydown", pressedKey)
